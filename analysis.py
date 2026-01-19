cde5ddcbb8a16a90727aa9b3ad1f5be4deaca4f6
Krithika RS <krithikars19@gmail.com>
2026-01-19 14:41:13 -0500

    Add farewell function

--- analysis.py
+++ analysis.py
@@ -1,5 +1,8 @@
 def greet(name):
     return f"Hello, {name}! Welcome to Git."
 
+def farewell(name):
+    return f"Goodbye, {name}. See you next time!"
 if __name__ == "__main__":
     print(greet("World"))
+    print(farewell("Student"))
